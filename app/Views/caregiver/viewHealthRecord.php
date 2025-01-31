3a2bc1cf76a3d357e140d336aa9162f7c99cf78a
Juanatas-Cris-Gabriel <juanatascris1@gmail.com>
2025-01-31 22:24:32 +0800

    commited by cris

--- app/Views/caregiver/viewHealthRecord.php
+++ app/Views/caregiver/viewHealthRecord.php
@@ -56,6 +56,13 @@
                                     </a>
                                 </td>
                             </tr>
-
+                    <!-- Edit Modal -->
+                    <div class="modal fade" id="editModal<?= $record['id'] ?>" tabindex="-1" aria-labelledby="editModalLabel<?= $record['id'] ?>" aria-hidden="true">
+                                <div class="modal-dialog">
+                                    <div class="modal-content">
+                                        <div class="modal-header">
+                                            <h5 class="modal-title" id="editModalLabel<?= $record['id'] ?>">Edit Health Record</h5>
+                                            <button type="button" class="btn-close" data-bs-dismiss="modal" aria-label="Close"></button>
+                                        </div>
                   
 </div>
