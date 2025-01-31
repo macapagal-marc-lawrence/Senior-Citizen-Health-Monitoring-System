a1dfee284873fc914ba34d25185e48c36d34937e
Venasquez-Charles-Harold-Justine <charlot1435@gmail.com>
2025-01-31 21:09:19 +0800

    "commited by charles"

---
+++ app/Views/admin/medicalHistory.php
@@ -0,0 +1,19 @@
+<?= $this->extend('layouts/frontend.php') ?>
+
+<?= $this->section('content') ?>
+
+<style>
+     .sidebar {
+        width: 70px;
+        background: rgba(233, 244, 248, 0.9);
+        color: #2d3748;
+        transition: width 0.3s;
+        position: fixed;
+        top: 0;
+        bottom: 0;
+        left: 0;
+        overflow: hidden;
+        border-right: 1px solid #cbd5e0;
+        z-index: 100;
+     }
+</style>
