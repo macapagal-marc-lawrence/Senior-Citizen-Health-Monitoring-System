dfe287809481c89d15e971e402c14918e8010631
Juanatas-Cris-Gabriel <juanatascris1@gmail.com>
2025-01-31 21:28:37 +0800

    added health records in views healthrecord.php pending

--- app/Views/caregiver/viewHealthRecord.php
+++ app/Views/caregiver/viewHealthRecord.php
@@ -13,3 +13,29 @@
         </div>
     </div>
 </nav>
+
+<div class= "container mt-4">
+    
+    
+        <h2 class= "header-text text-center">Health & Medication Records for <?=$senior ['name']?></h2>
+
+        <div class="row">
+
+        <!-- Health Records -->
+        <div class="col-md-12 table-responsive">
+            <h4 class="text-primary">Health Records</h4>
+            <?php if (count($healthRecords) > 0): ?>
+                <table class="table table-striped table-bordered table-sm">
+                    <thead class="thead-light">
+                        <tr>
+                            <th style="width: 15%;">Health Condition</th>
+                            <th style="width: 25%;">Description</th>
+                            <th style="width: 10%;">Temperature</th>
+                            <th style="width: 10%;">Blood Pressure</th>
+                            <th style="width: 10%;">Heart Rate</th>
+                            <th style="width: 15%;">Record Date</th>
+                            <th style="width: 15%;">Actions</th>
+                        </tr>
+                    </thead>
+                  
+</div>
