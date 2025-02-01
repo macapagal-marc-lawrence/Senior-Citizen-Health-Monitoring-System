fb0b4515d097e3e0d2f78a2073100cde451a0095
Juanatas-Cris-Gabriel <juanatascris1@gmail.com>
2025-02-01 12:54:27 +0800

    nag base po ako sa models ng mga ka group ko

---
+++ app/Controllers/Caregiver.php
@@ -0,0 +1,11 @@
+<?php
+namespace App\Controllers;
+
+use App\Models\HealthRecordModel;
+use App\Models\UserModel;
+use App\Models\MedicationModel;
+use App\Models\MedicationHistoryModel; 
+use App\Models\HealthHistoryModel;
+use App\Models\ReminderModel;
+use App\Models\SeniorCaregiverModel; 
+use CodeIgniter\Controller;
