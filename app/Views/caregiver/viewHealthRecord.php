28f96e7f89657917c424f5a2269663b872af9c08
Juanatas-Cris-Gabriel <juanatascris1@gmail.com>
2025-02-01 12:40:57 +0800

    modal

--- app/Views/caregiver/viewHealthRecord.php
+++ app/Views/caregiver/viewHealthRecord.php
@@ -141,4 +141,14 @@
                                     </a>
                                 </td>
                             </tr>
+
+                            
+                            <!-- Edit Modal -->
+                            <div class="modal fade" id="editModalMed<?= $medication['id'] ?>" tabindex="-1" aria-labelledby="editModalLabelMed<?= $medication['id'] ?>" aria-hidden="true">
+                                <div class="modal-dialog">
+                                    <div class="modal-content">
+                                        <div class="modal-header">
+                                            <h5 class="modal-title" id="editModalLabelMed<?= $medication['id'] ?>">Edit Medication Record</h5>
+                                            <button type="button" class="btn-close" data-bs-dismiss="modal" aria-label="Close"></button>
+                                        </div>
 </div>
